71f4b8a6275f2adda4d952fd17abe579b190ff06
unknown <vijay.srj@gmail.com>
2020-04-19 20:08:02 +0530

    sqlite-springboot-integration

---
+++ src/main/java/com/springboot/sqlite/SqliteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.sqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqliteApplication.class, args);
+	}
+
+}
